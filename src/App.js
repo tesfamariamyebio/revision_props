f801811d76ae25f5f779a285c43a282d1607ba84
tesfamariamyebio <65938837+tesfamariamyebio@users.noreply.github.com>
2020-08-17 18:09:04 +0200

    'mypropsrevision'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Day from './components/Day';
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+// import './App.css';
+
+class App extends React.Component{
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="container-fluid">
+  <div className='row'>
+  <div className='colo-4'>
+
+  <Day 
+  dayName='Thursday' 
+  dayNumber='8'
+  monthName='July'/>
+  </div>
+  <div className='colo-4 bg-success text-white border'>
+  <Day dayName='Friday' 
+  dayNumber='9'
+  monthName='July'
+  />
+  </div>
+  <div className='colo-4  bg-danger text-white'>
+  <Day
+  dayName="Saturday"
+  dayNumber='10'
+  monthName="July"
+   />
+   </div>
+  </div>
+</div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Day.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+class Day extends React.Component{
+    render(){
+        return (
+            <div className='container-fluid' style={{width:"300",height:"400",BackgroundColor:"red"}}>
+                
+                <h1>{this.props.dayName} </h1>
+                <h2>{this.props.dayNumber}</h2>
+                <h3>{this.props.monthName}</h3>
+                
+            
+            </div>
+        );
+    }
+
+}
+
+Day.propTypes={
+    dayName:PropTypes.string,
+    dayNumber:PropTypes.number,
+    monthName:PropTypes.string,
+
+};
+
+Day.defaultProps={
+dayName:'Someday',
+dayNumber:'-1',
+monthName:'July',
+
+};
+export default Day;
